4fe607ce7adf99733834fffc3a5bf882fa3fbb40
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-07-30 22:06:57 -0300

    Add files via upload

---
+++ Atividade 01 - php introdução/exer2.php
@@ -0,0 +1,13 @@
+<?php
+
+    $primeiro = 1;
+    $segundo = 100;
+    $soma = 0;
+    while($primeiro <= 100){
+        $soma = $soma + $primeiro;
+        $primeiro++;
+    }
+    
+    echo "Soma: " . $soma;
+
+?>
---
+++ Atividade 01 - php introdução/exer3.php
@@ -0,0 +1,11 @@
+<?php 
+
+    echo "Divisíveis por 3: ";
+    echo "<br>";
+    $n1 = 1;
+    for($n1=1; $n1<=50; $n1++){
+        if($n1 % 3 == 0)
+            echo $n1 . "<br>";
+    }
+
+?>
