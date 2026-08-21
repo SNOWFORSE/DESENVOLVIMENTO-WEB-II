cfc4d64682c2a769f96ced0f20804291de577295
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-08-20 21:21:55 -0300

    Add HTML form for football team submission

---
+++ Aula - 6 | php/formularios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    
+    <h4>Futebol</h4>
+
+    <form action="formulario_exec.php" method="POST">
+
+        <input type="text" placeholder="Nome do Time" name="Nome">
+
+        <br><br>
+
+        <input type="text" placeholder="Cidade do Time" name="Cidade">
+
+        <br><br>
+
+        <button>Enviar</button>
+
+    </form>
+
+</body>
+</html>
